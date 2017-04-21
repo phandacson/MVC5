fd6c040073ea03d56f174736e564b80909752f3d
Phan Dac Son <phandacson@live.com>
2017-04-21 17:18:51 +0700

    Init

---
+++ MVC5-Demo2/MVC5-Demo2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5_Demo2.MvcApplication" Language="C#" %>
---
+++ MVC5-Demo2/MVC5-Demo2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5_Demo2.Startup))]
+namespace MVC5_Demo2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
